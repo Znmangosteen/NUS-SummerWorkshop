2ba7b3e7857b435c8f3054ce55d8179b0b53cdca
Znmangosteen <Znmangosteen@hotmail.com>
2019-07-24 22:21:11 +0800

    modify load state

--- Project/public_html/src/Engine/Utils/LoadingIconConfig.js
+++ Project/public_html/src/Engine/Utils/LoadingIconConfig.js
@@ -10,13 +10,13 @@
 var gEngine = gEngine || { };
 
 gEngine.LoadingIconConfig = (function () {
-    
+
     /**
      * A varuable used to keep track of total # of loads necessary for the laoding bar
      * @memberOf gEngine.LoadingIconConfig
      */
     var levelLoadCount=0;
-    
+
     /**
      * Creates the necessary elements needed for the loading screen
      * @memberOf gEngine.LoadingIconConfig
@@ -26,7 +26,10 @@ var setup = function(){
     var cheight=document.getElementById("LoadingIconParent").style.height;
     document.getElementById("GLCanvas").width=cwidth.substr(0,cwidth.length - 2);
     document.getElementById("GLCanvas").height=cheight.substr(0,cheight.length - 2);
-    document.write("<div class='LoadingSpinnerAnimation' id='LoadingSpinner'></div>");
+    // document.write("<div class='LoadingSpinnerAnimation' id='LoadingSpinner'></div>");
+    document.write("<div class='loading' id='loading'>\n" +
+        "        <span></span>\n" +
+        "</div>\n");
     document.write("<p class='LoadingDotsAnimation' id='LoadingDots'>Loading<span>.</span><span>.</span><span>.</span></p>");
     document.write("<div id='LoadingScreenProgress'><div id='LoadingScreenBar'>0%</div></div>");
 };
@@ -36,7 +39,8 @@ var setup = function(){
      * @memberOf gEngine.LoadingIconConfig
      */
 var start = function(){
-    document.getElementById("LoadingSpinner").style.display = "block";
+    // document.getElementById("LoadingSpinner").style.display = "block";
+    document.getElementById("loading").style.display = "block";
     document.getElementById("LoadingDots").style.display="block";
     document.getElementById("LoadingScreenBar").style.display="block";
     document.getElementById("LoadingScreenProgress").style.display="block";
@@ -50,7 +54,8 @@ var stop = function(){
     levelLoadCount=0;
     document.getElementById("LoadingScreenBar").style.width="0%";
     document.getElementById("LoadingScreenBar").innerHTML = "0%";
-    document.getElementById("LoadingSpinner").style.display="none";
+    // document.getElementById("LoadingSpinner").style.display="none";
+    document.getElementById("loading").style.display="none";
     document.getElementById("LoadingDots").style.display="none";
     document.getElementById("LoadingScreenBar").style.display="none";
     document.getElementById("LoadingScreenProgress").style.display="none";
@@ -68,7 +73,7 @@ var loadingUpdate = function() {
 var loadCountReset = function() {
         levelLoadCount = 0;
     };
-    
+
     /**
      * Checks if a level is currently loading
      * @memberOf gEngine.LoadingIconConfig
@@ -76,7 +81,7 @@ var loadCountReset = function() {
     var isLevelSet = function() {
         return levelLoadCount!==0;
     };
-    
+
     /**
      * Returns the number of loads needed for the level
      * @memberOf gEngine.LoadingIconConfig
@@ -84,7 +89,7 @@ var loadCountReset = function() {
     var getLevelLoadCount = function() {
         return levelLoadCount;
     };
-    
+
     /**
      * Sets the progress bar amount to the number of loads needed for the level
      * @memberOf gEngine.LoadingIconConfig
