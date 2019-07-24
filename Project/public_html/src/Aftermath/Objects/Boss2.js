a3ef0c038521efb1c14b075b65eee59e7d38273d
Znmangosteen <Znmangosteen@hotmail.com>
2019-07-24 09:27:10 +0800

    update boss

--- Project/public_html/src/Aftermath/Objects/Boss2.js
+++ Project/public_html/src/Aftermath/Objects/Boss2.js
@@ -63,7 +63,7 @@ function Boss2(spriteTexture, bullet) {
     //Rate in per second
     this.kfireRate = 5;
 
-    this.health = 2;
+    this.health = 10;
     this.death = false;
 
     GameObject.call(this, this.mBoss2);
